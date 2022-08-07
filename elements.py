e8ceba90e348ff226975274150e1a75cd2508ab5
Paulo Arthur <pauloart.hur670@gmail.com>
2022-08-07 20:54:45 -0300

    uploading files

---
+++ elements.py
@@ -0,0 +1,32 @@
+import random
+from time import time
+
+def spaceships(WIDTH, HEIGHT):
+    return {
+        'x': WIDTH/2 - 30/2,
+        'y': HEIGHT - 30 - 30,
+        'size': [30, 30],
+        'speed': 0.5,
+        'recoil': 0.8
+    }
+
+def get_ammo(x, y, time):
+    return {
+        'x': x,
+        'y': y,
+        'size': 10,
+        'speed': 1,
+        'time': time,
+        'color': (120, 255, 120)
+    }
+
+def get_enemy(WIDTH, HEIGHT, n):
+    return {
+        'x': random.randint(0.1 * WIDTH, 0.9 * WIDTH),
+        'y': 30 + random.randint(0, HEIGHT * 0.2),
+        'size': [30, 30],
+        'speed': random.choice([-1, 1]) * 0.5,
+        'recoil': 0.8,
+        'last_shot': time(),
+        'color': (random.randint(150, 230), 70, random.randint(60, 140))
+    }
---
+++ main.py
@@ -0,0 +1,117 @@
+import pygame, random, sys
+from time import time
+from elements import spaceships, get_ammo, get_enemy
+
+pygame.init()
+#pygame.font.get_init()
+
+pygame.display.set_caption('Space Invaders')
+
+WIDTH = 1000
+HEIGHT = 600
+BLACK = 50, 40, 70
+WHITE = 220, 220, 220
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+spaceship = spaceships(WIDTH, HEIGHT)
+
+ammo = []
+enemies = {'number': 5, 'wave_time': 5, 'enemies': []}
+enemy_ammo = []
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    #screen
+    screen.fill(BLACK)
+
+    keys = pygame.key.get_pressed()
+
+    #spaceship control & limits
+    if keys[pygame.K_w] and spaceship['y'] >= 0:
+        spaceship['y'] -= spaceship['speed']
+    if keys[pygame.K_s] and spaceship['y'] <= HEIGHT - spaceship['size'][1]:
+        spaceship['y'] += spaceship['speed']
+    if keys[pygame.K_a] and spaceship['x'] >= 0:
+        spaceship['x'] -= spaceship['speed']
+    if keys[pygame.K_d] and spaceship['x'] <= WIDTH - spaceship['size'][0]:
+        spaceship['x'] += spaceship['speed']
+
+    #ammo
+    if keys[pygame.K_SPACE]:
+        if len(ammo) == 0 or time() - ammo[-1]['time'] > spaceship['recoil']:
+            ammo.append(get_ammo(spaceship['x'] + spaceship['size'][0]/2 - 5, spaceship['y'], time()))
+
+    for bullet in ammo:
+        pygame.draw.rect(screen, bullet['color'], pygame.Rect(bullet['x'],
+                                                    bullet['y'],
+                                                    bullet['size'],
+                                                    bullet['size']))
+        bullet['y'] -= bullet['speed']
+
+        if bullet['y'] < -10:
+            del ammo[ammo.index(bullet)]
+
+    #spaceship
+    pygame.draw.rect(screen, WHITE, pygame.Rect(spaceship['x'],
+                                                spaceship['y'],
+                                                spaceship['size'][0],
+                                                spaceship['size'][1]))
+
+    #enemies
+    if len(enemies['enemies']) == 0:
+        for _ in range(0, enemies['number']):
+            enemies['enemies'].append(get_enemy(WIDTH, HEIGHT, enemies['number']))
+
+    for enemy in enemies['enemies']:
+        enemy_rect = pygame.Rect(enemy['x'],
+                                 enemy['y'],
+                                 enemy['size'][0],
+                                 enemy['size'][1])
+
+        pygame.draw.rect(screen, enemy['color'], enemy_rect)
+        enemy['x'] += enemy['speed']
+
+        #enemy collision
+        if enemy['x'] >= WIDTH - enemy['size'][0] or enemy['x'] <= 0:
+            enemy['speed'] *= -0.1 * random.randint(8, 12)
+
+        #enemy's_shot
+        if time() - enemy['last_shot'] >= 4:
+            enemy_bullet = get_ammo(enemy['x'], enemy['y'], None)
+            enemy_bullet['speed'] *= 0.5
+            enemy_ammo.append(enemy_bullet)
+            enemy['last_shot'] = time()
+
+        #enemy's death
+        bullet_list = []
+        for bullet in ammo:
+            bullet_list.append(pygame.Rect(
+                bullet['x'], bullet['y'], bullet['size'], bullet['size']
+            ))
+        enemy_death = enemy_rect.collidelist(bullet_list)
+
+        if enemy_death != -1:
+            del enemies['enemies'][enemies['enemies'].index(enemy)]
+            del ammo[enemy_death]
+
+    #enemies's shots
+    for bullet in enemy_ammo:
+        pygame.draw.rect(screen, WHITE, pygame.Rect(
+            bullet['x'], bullet['y'], bullet['size'], bullet['size']
+        ))
+        bullet['y'] += bullet['speed']
+
+        spaceship_death = pygame.Rect(bullet['x'], bullet['y'], bullet['size'], bullet['size']).collidelist([pygame.Rect(spaceship['x'],
+                                                    spaceship['y'],
+                                                    spaceship['size'][0],
+                                                    spaceship['size'][1])])
+
+        if spaceship_death != -1:
+            break
+    pygame.display.update()
+
+input()
